661d7dc1d0d7113e8e0b418bc3915734be5636ed
NoorTantawi <altntawyn@gmail.com>
2025-06-02 13:33:16 +0300

    git commands

--- obj/Debug/net6.0/UML_Project.AssemblyInfo.cs
+++ obj/Debug/net6.0/UML_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UML_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87c2d0eb699ba834063a0f6558408dea88f5b6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UML_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UML_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
